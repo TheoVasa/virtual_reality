f89992e5e5063e8cae1125b78b3c98f58e5e1fd6
irisdf <iris.defromont@epfl.ch>
2025-05-11 21:14:32 +0200

    Bots animation

---
+++ Assets/BotCorridorAsset/Scripts2/ShutDownLight.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class ShutDownLight : MonoBehaviour
+{
+    public GameObject BotCollider; //Collider of the corresponding Bot
+    public GameObject Light; //The Spot Light of the corresponding Bot
+    private bool BotColliderisActive; //If the Bot Box collider is active or not
+
+    // Update is called once per frame
+    void Update()
+    {
+        BotColliderisActive = BotCollider.activeSelf; //Each frame check if the collider is active or not
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //if the ball enter in the collider of the switch then desactivate the spot light and the box collider of the bot
+        if (other.CompareTag("MetallicBall") && BotColliderisActive)
+        {
+            print("Touch the switch");
+            print("Desactivate the light and the detection area of the bot");
+            Light.SetActive(false);
+            BotCollider.SetActive(false);
+        }
+    }
+}
